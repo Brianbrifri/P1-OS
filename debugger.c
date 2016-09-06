dcfc1883bbbcf632e5619b8b9277c0ea2fd3e891
Brian K <brianbrifri@gmail.com>
2016-09-06 15:53:27 -0500

    trying to fix debugger.c

--- debugger.c
+++ debugger.c
@@ -12,23 +12,20 @@ char *fileName = NULL;
 
 int main (int argc, char **argv)
 {
-  int index;
   int hflag = 0;
+  int index;
   int nValue = 42;
-  fileName = "logfile.txt";
+  char *filename = "logfile.txt";
   char *option = NULL;
-  char *programName = argv[0];
   char *short_options = "hn:l:";
   int c;
   struct option long_options[] = {
     {"help", no_argument, 0, 'h'},
-    {0,      0,           0,  0},
     {}
   };
 
   opterr = 0;
   while ((c = getopt_long (argc, argv, short_options, long_options, NULL)) != -1)
-    printf("Top of while loop\n");
     switch (c)
       {
       case 'h':
@@ -38,16 +35,22 @@ int main (int argc, char **argv)
         nValue = atoi(optarg);
         break;
       case 'l':
-        fileName = optarg;
+        filename = optarg;
         break;
       case '?':
         if (optopt == 'n') {
           fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        } 
+        }
         else if (optopt == 'l')
           fprintf (stderr, "Option -%c requires an argument.\n", optopt);
         else if (isprint (optopt)) {
           fprintf (stderr, "Unknown option -%c.\n", optopt);
+          data_t newData;
+          newData.string = "Unknown option -c\n";
+          if(addMsg(newData) == 0) {
+            printf("success\n");
+          }
+          saveLog(filename);
         }
         else
           fprintf (stderr,
@@ -57,7 +60,10 @@ int main (int argc, char **argv)
       default:
         abort ();
       }
-  
+
+  printf ("hflag = %d, nValue = %d, filename = %s\n",
+          hflag, nValue, filename);
+
   for (index = optind; index < argc; index++)
     printf ("Non-option argument %s\n", argv[index]);
 
